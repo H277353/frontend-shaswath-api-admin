9a410ac77ec75fcf32284b3c993e83eecaec8e68
Nagu <154983315+H277353@users.noreply.github.com>
2025-10-28 17:39:06 +0530

    placeholder tables added

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,13 @@ import ProductTable from './components/Tables/ProductTable'
 import SubProductTable from './components/Tables/SubProductTable'
 import OperatorTable from './components/Tables/OperatorTable'
 import SubOperatorTable from './components/Tables/SubOperatorTable'
+import BankTable from './components/Tables/BankTable'
+import NetworkBankTable from './components/Tables/NetworkBankTable'
+import VendorTable from './components/Tables/VendorTable'
+import VendorCredentialTable from './components/Tables/VendorCredentialTable'
+import VendorRoutingTable from './components/Tables/VendorRoutingTable'
+
+
 
 function App() {
   
@@ -17,13 +24,22 @@ function App() {
         {/* The dashboard page will render in the <Outlet /> */}
         <Route index element={<Dashboard />} />
 
-        {/* Example for future pages */}
+        {/* Master pages */}
         <Route path="master" >
           <Route path="products" element={<ProductTable />} />
           <Route path="sub-product" element={<SubProductTable />} />
           <Route path="operator" element={<OperatorTable />} />
-          <Route path="sub-operator" element={<SubOperatorTable />} />
+          <Route path="sub-operator" element={<SubOperatorTable /> } />
+          <Route path="vendor" element={<VendorTable />} />
+          <Route path="vendor-credential" element={<VendorCredentialTable />} />
+          <Route path="vendor-routing" element={<VendorRoutingTable />} />
+          <Route path="banks" element={<BankTable />} />
+          <Route path="network-banks" element={<NetworkBankTable />} />
+          
+          
         </Route>
+
+        <Route path="*" element={<Dashboard /> } />
       
       </Route>
 
---
+++ src/components/Tables/VendorCredentialTable.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const VendorCredentialTable = () => {
+  return (
+    <div>VendorCredentialTable</div>
+  )
+}
+
+export default VendorCredentialTable
